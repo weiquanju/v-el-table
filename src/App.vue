f02ccf2ebae1df5b1939d69d6f94a97fb0aa1d22
weiquanju <anbine@qq.com>
2022-01-11 09:49:54 +0800

    fix: 优化组件参数

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
 import Form from './components/form'
 import { FormProps } from './components/form/interfaces'
 import { reactive } from 'vue';
+import { ElInput } from 'element-plus';
 const config: FormProps = reactive({
-  form: { model: { name: 'zhangsan' } },
+  form: { model: { name: 'zhangsan', num: 1 } },
   fields: [
     {
       itemProps: { prop: 'name', label: '姓名' },
       inputComponent: 'el-input',
+      inputProps: { type: 'text', placeholder: 'Please input' },
+      inputEvents: {
+        change: (...args: any) => console.log(...args)
+      }
+    },
+    {
+      itemProps: { prop: 'name', label: '姓名' },
+      inputComponent: ElInput,
       inputProps: { type: 'text', placeholder: 'Please input' },
       inputEvents: {
         change: (...args: any) => console.log(...args)
