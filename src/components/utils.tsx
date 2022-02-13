5350fd5e9073fcb09fc54c5613a0ed3eba482c44
weiquanju <anbine@qq.com>
2022-02-13 13:39:07 +0800

    feat: 增加函数：转为驼峰命名、重置变量值

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -17,3 +17,35 @@ export const eventsTransform = (handlers?: EventsHandlers) => {
   Object.entries(handlers).forEach(([key, handler]) => map.set(`on${key.charAt(0).toUpperCase()}${key.substring(1)}`, handler))
   return Object.fromEntries(map)
 }
+
+export const toCamelCase = (str: string) => {
+  return str.replace(/[-_ ](\w)|(^\w)/g, (all, one) => {
+    return !one ? all.toLowerCase() : one.toUpperCase()
+  })
+}
+
+export const toCamelCaseProp = (props: { [key: string]: any }) => {
+  const propKeys = Object.keys(props)
+  return Object.fromEntries(
+    propKeys.map((key: string) => {
+      const value = props[key]
+      if (propKeys.find((key) => /-/.test(key))) {
+        return [toCamelCase(key), value]
+      }
+      return [key, value]
+    }),
+  )
+}
+
+export const resetValue = (formData: any) => {
+  for (const key in formData) {
+    // object类型处理，避免object类型被置为undifined，避免业务层因数据类型改变出现异常的情况
+    if (formData[key] !== null && typeof formData[key] === 'object') {
+      //数组、对象、函数类型
+      formData[key] = new formData[key].constructor()
+      continue
+    }
+    formData[key] = undefined
+  }
+  return formData
+}
