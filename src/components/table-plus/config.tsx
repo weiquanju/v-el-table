8b6524cdaf9cb5ab5b689553b8b794ca583f846c
weiquanju <anbine@qq.com>
2022-02-14 10:01:39 +0800

    feat: 分页默认配置常量

---
+++ src/components/table-plus/config.tsx
@@ -0,0 +1,9 @@
+// 存放默认配置常量
+
+export const paginationDefault = {
+  currentPage: 1,
+  pageSizes: [10, 20, 30, 40, 50],
+  pageSize: 10,
+  layout: 'total, sizes, prev, pager, next, jumper',
+  total: 0,
+}
