7c79897ed924ec0e5a3876589ac118eeed0bf3ac
weiquanju <anbine@qq.com>
2022-02-15 02:15:28 +0800

    feat: 分页参数存放路径配置

--- src/components/tablePlus/config.tsx
+++ src/components/tablePlus/config.tsx
@@ -1,9 +1,17 @@
 // 存放默认配置常量
 
+import { ref } from 'vue'
+
 export const paginationDefault = {
   currentPage: 1,
   pageSizes: [10, 20, 30, 40, 50],
   pageSize: 10,
   layout: 'total, sizes, prev, pager, next, jumper',
   total: 0,
+}
+// 分页参数存放路径配置
+export const dataPath = {
+  data: 'data',
+  currentPage: 'page',
+  total: 'total',
 }
