f696d69c1131f2fa8bccaaa77247b9a2d6a8a1d8
weiquanju <anbine@qq.com>
2022-02-16 04:03:53 +0800

    feat: 加入核心组件

--- src/components/install.ts
+++ src/components/install.ts
@@ -1,9 +1,15 @@
-import { App } from 'vue'
+import { App, FunctionalComponent } from 'vue'
 import HelloWorld from './HelloWorld.vue'
+import Form from './form'
+import Table from './table'
+import TablePlus from './tablePlus'
 
 export default {
   install: (app: App<Element>, options: any) => {
     app.provide('HelloWorld', options)
     app.component('HelloWorld', HelloWorld)
+    app.component('VElForm', Form)
+    app.component('VElTable', Table as FunctionalComponent)
+    app.component('VElTablePlus', TablePlus as FunctionalComponent)
   },
 }
