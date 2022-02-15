e0dc3c2dc29628e98c5860e18bf8d04bfb608164
weiquanju <anbine@qq.com>
2022-02-16 04:03:05 +0800

    feat: 支持lib打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/install.ts'),
+      name: 'v-el-table',
+      fileName: (format) => `v-el-table.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue', 'element-plus', '@element-plus/icons-vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+          'element-plus': 'ElementPlus',
+          '@element-plus/icons-vue': 'ElementPlusIcon',
+        },
+      },
+    },
+  },
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
