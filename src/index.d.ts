63d30bff2e39d5678e41fd9fa78d9a63f166e05f
weiquanju <anbine@qq.com>
2022-02-16 05:57:39 +0800

    fix: 修复type导出对象

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,10 @@
 import { App } from 'vue'
-import VElFrom, { ElFormProps, ElFormItemProps, FormProps, ComponentName } from './components/form/interfaces'
-import TableBasic, { FormatterArgArray, FormatterArgObject, TableBasicProps, ColumnSlots, Slots, Slot } from './components/table/TableType'
-import TablePlus, { TablePlusProps, QueryFnType, DataPath, PaginationProps } from './components/tablePlus/tablePlusType'
-export declare namespace VElTable {
-  export default TableBasic
-  export { FormatterArgArray, FormatterArgObject, TableBasicProps, ColumnSlots, Slots, Slot }
-}
+import VElFrom from './components/form/interfaces'
+import VElTable from './components/table/TableType'
+import VElTablePlus from './components/tablePlus/tablePlusType'
 
-export declare namespace VElFrom {
-  export default VElFrom
-  export { ElFormProps, ElFormItemProps, FormProps, ComponentName }
-}
+function install(app: App<Element>, options: any): void
 
-export declare namespace VElTablePlus {
-  export default TablePlus
-  export { TablePlusProps, QueryFnType, DataPath, PaginationProps }
-}
+export default { install }
 
 export { VElTable, VElFrom, VElTablePlus }
-
-export default (app: App<Element>, options: any) => void 0
