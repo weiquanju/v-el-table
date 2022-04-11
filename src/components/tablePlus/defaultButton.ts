45975f7ef0052aee5994b0ddb607220b649cc678
weiquanju <anbine@qq.com>
2022-04-11 13:04:13 +0800

    feat: 按钮支持国际化

--- src/components/tablePlus/defaultButton.ts
+++ src/components/tablePlus/defaultButton.ts
@@ -1,28 +1,29 @@
-import { ElButton } from 'element-plus'
 import * as Icons from '@element-plus/icons-vue'
 import { TablePlusProps } from './index.d'
 import { resetValue, eventsTransform } from '../utils'
-import { h } from 'vue'
+import { ElButton } from 'element-plus'
+import { h, isRef, Ref } from 'vue'
+import I18N from '../I18N'
 
 export type ButtonConfig = {
-  name: string
+  name: string | Ref<string>
   icon: keyof typeof Icons
   events?: any
 }
 
+/**@todo 定义按钮组件 */
+// export type ButtonComponentc  cx               c
 export const getDefaultButtons = ({ props, query }: { props: TablePlusProps; query: () => void }) => {
-  const iconStyle = { width: '1em', height: '1em', marginRight: '0.3em' }
-
   const buttons: ButtonConfig[] = [
     {
-      name: 'query',
+      name: I18N.t('query'),
       icon: 'Search',
       events: {
         click: query,
       },
     },
     {
-      name: 'reset',
+      name: I18N.t('reset'),
       icon: 'CircleClose',
       events: { click: () => resetValue(props.formProps.form.model) },
     },
@@ -45,8 +46,8 @@ export const getDefaultButtons = ({ props, query }: { props: TablePlusProps; que
   ]
   return buttons.map(({ name, icon, events = {} }) =>
     h(ElButton, eventsTransform(events), {
-      icon: () => h(Icons[icon], { style: iconStyle }),
-      default: () => name,
+      icon: () => h(Icons[icon]),
+      default: () => (isRef(name) ? name.value : name),
     }),
   )
 }
