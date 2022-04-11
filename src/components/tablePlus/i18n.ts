4b4e5528483d8278c353998e05b9abab36417c22
weiquanju <anbine@qq.com>
2022-04-11 12:54:41 +0800

    feat: 使用ElementPlus语言配置

---
+++ src/components/tablePlus/i18n.ts
@@ -0,0 +1,17 @@
+import { configProviderContextKey, useGlobalConfig } from 'element-plus'
+import { Ref, watchEffect, WatchOptionsBase, WatchStopHandle } from 'vue'
+import I18N from '../I18N'
+
+/*使用ElementPlus语言配置*/
+const lang: Ref<string> = useGlobalConfig(configProviderContextKey as any, 'en')
+
+// 加载后，开始监听
+let stopHandle: WatchStopHandle | undefined = undefined
+if (!stopHandle) {
+  stopHandle = watchEffect(
+    () => {
+      I18N.setLocale(lang.value)
+    },
+    { immediate: true } as WatchOptionsBase,
+  )
+}
