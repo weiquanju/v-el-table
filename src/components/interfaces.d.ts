473263b1e38566312762324592e4acd73cb46764
weiquanju <anbine@qq.com>
2022-04-12 10:44:48 +0800

    feat: 新增Writable

--- src/components/interfaces.d.ts
+++ src/components/interfaces.d.ts
@@ -9,3 +9,5 @@ export type EventsHandlers = Record<string, (...args: any[]) => void>
 export type ComponentType = string | Component | DefineComponent | JSX.Element | ConcreteComponent | FunctionalComponent
 
 export type Setup = <Props = any, Data = object>(props: Readonly<Props>, ctx: SetupContext) => RenderFunction | Data
+
+export type Writable<T> = { -readonly [P in keyof T]: T[P] }
