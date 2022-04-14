e69cf5edbcc480de7108bba34c4a8311fdae492e
weiquanju <anbine@qq.com>
2022-04-15 00:07:14 +0800

    fix: 修复options参数

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,11 +5,11 @@ import VElTable from './table'
 import VElTablePlus from './tablePlus'
 
 export default {
-  install: (app: App<Element>, options: any = {}) => {
+  install: (app: App<Element>, options?: any) => {
     app.component('VElForm', VElForm)
     app.component('VElTable', VElTable as FunctionalComponent)
     app.component('VElTablePlus', VElTablePlus as FunctionalComponent)
-    if (typeof options.lang === 'string') {
+    if (typeof options?.lang === 'string') {
       lang.value = options.lang
     }
   },
