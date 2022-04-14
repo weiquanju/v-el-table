fc31661cdbb44d130bcb2b27a30c9046277b880b
weiquanju <anbine@qq.com>
2022-04-14 22:17:09 +0800

    fix: 优化打包依赖

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus', '@element-plus/icons-vue'],
+      external: ['vue', 'element-plus', '@element-plus/icons-vue', 'element-plus/lib/components/index'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
