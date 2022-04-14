4c4e87d61cec61faf4589bde660070971ba8d86a
weiquanju <anbine@qq.com>
2022-04-14 22:17:36 +0800

    fix: 修复类型引用地址错误

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,6 +1,6 @@
 import { App } from 'vue'
 import VElFrom from './form/interfaces'
-import VElTable from './table/TableType'
+import VElTable from './table/index.d'
 import VElTablePlus from './tablePlus/index'
 
 interface VElTablePlugin {
