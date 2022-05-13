66aac8022c5c4bc4b3040a4e7b4c9eb41f635d4f
weiquanju <anbine@qq.com>
2022-05-14 00:14:04 +0800

    fix: fix bug ,  get local configure faild

--- src/components/i18nInit.ts
+++ src/components/i18nInit.ts
@@ -1,9 +1,10 @@
-import { configProviderContextKey, useGlobalConfig } from 'element-plus'
-import { Ref, watchEffect, WatchOptionsBase, WatchStopHandle } from 'vue'
+import { useGlobalConfig } from 'element-plus'
+import { ref, Ref, toRef, watchEffect, WatchOptionsBase, WatchStopHandle } from 'vue'
 import I18N from './I18N'
 
+const local = (useGlobalConfig('locale') as Ref<{ name: string }> | undefined) || ref({ name: 'en' })
 /*使用ElementPlus语言配置*/
-export const lang: Ref<string> = useGlobalConfig(configProviderContextKey as any, 'en')
+export const lang: Ref<string> = toRef(local.value, 'name')
 
 // 加载后，开始监听
 let stopHandle: WatchStopHandle | undefined = undefined
